ed7d9fbe3a7c0d701cf779b73314ae0c5885ab82
ZakharB14 <zakharb713@gmail.com>
2024-10-09 16:13:46 +0500

    upd

--- server/utils/checkAuth.js
+++ server/utils/checkAuth.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
-  const token = req.headers.Authorization;
+  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
   if (token) {
     try {
       const decoded = jwt.verify(token, "secret123");
-
       req.userId = decoded._id;
+      
       next();
     } catch (err) {
       console.log(err);
