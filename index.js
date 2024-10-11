f6ba649c7ee02e8a018192150db4327456b18575
zborodayev1 <zakharb713@gmail.com>
2024-10-11 22:14:26 +0500

    sinc front end and back end

--- index.js
+++ index.js
@@ -57,7 +57,9 @@ app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
     url: `/uploads/${req.file.originalname}`,
   });
 });
+app.get('/tags', PostController.getLastTags)
 app.get("/posts", PostController.getAll);
+app.get("/posts/tags", PostController.getLastTags);
 app.get("/posts/:id", PostController.getOne);
 app.post(
   "/posts",
